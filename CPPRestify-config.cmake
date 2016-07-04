1d57a7a210b152dee5619577b3da4cc6ac735889
Christoph Heindl <christoph.heindl@gmail.com>
2016-07-04 13:32:54 +0200

    Adding lib vars.

--- CPPRestify-config.cmake
+++ CPPRestify-config.cmake
@@ -1,3 +1,4 @@
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(${SELF_DIR}/cpp-restify-targets.cmake)
 get_filename_component(CPPRestify_INCLUDE_DIRS "${SELF_DIR}/../../inc/" ABSOLUTE)
+set(CPPRestify_LIBRARIES cpp-restify)
--- vendor/jsoncpp/JsonCPP-config.cmake
+++ vendor/jsoncpp/JsonCPP-config.cmake
@@ -1,3 +1,4 @@
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(${SELF_DIR}/jsoncpp-targets.cmake)
 get_filename_component(JsonCPP_INCLUDE_DIRS "${SELF_DIR}/../../inc/" ABSOLUTE)
+set(JsonCPP_LIBRARIES jsoncpp)
